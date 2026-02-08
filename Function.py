dc26dc096c4e53d3cc484460aaff0fce662edaba
Taras <arikcarikc4@gmail.com>
2026-02-08 11:30:22 +0200

    Changes

--- Function.py
+++ Function.py
@@ -1,3 +1,12 @@
+# 1
+def reverse_string(s: str) -> str:
+    if len(s) <= 1:
+        return s
+
+    return reverse_string(s[1:]) + s[0]
+
+
+# 2
 def revers(s, i=0):
     if len(s) <= 1:
         return s
